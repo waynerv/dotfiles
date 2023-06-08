101fc913be349f543d058180073468d1473a45fc
Waynerv <wei.xie@woqutech.com>
2023-06-08 12:35:30 +0800

    add lsp-zero for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -47,6 +47,27 @@ require("lazy").setup({
 
   "ggandor/leap.nvim",
 
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  },
+
 })
 
 -- Set colorscheme
@@ -82,4 +103,15 @@ require'nvim-treesitter.configs'.setup {
 require('lualine').setup()
 
 require('leap').add_default_mappings()
+
+-- LSP
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
 
