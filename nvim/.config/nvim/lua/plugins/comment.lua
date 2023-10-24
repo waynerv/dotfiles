416d1ba4a41bf6f83ef43501ef445f92e22e53fc
Waynerv <wei.xie@woqutech.com>
2023-10-24 11:09:26 +0800

    nvim: use keymap to lazy load Comment.nvim

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,7 +1,9 @@
 local M = {
   "numToStr/Comment.nvim",
   commit = "0236521ea582747b58869cb72f70ccfa967d2e89",
-  event = "VeryLazy",
+  keys = {
+    { "gc", mode = { "n", "x" } },
+  },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     {
