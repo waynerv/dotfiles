31f9eb98bb2a32caa5c1aa9bae22dc548e4c263c
Waynerv <wei.xie@woqutech.com>
2023-11-29 19:50:31 +0800

    nvim: increase treesitter highlight max_filesize to 1MB

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -26,7 +26,7 @@ return {
         highlight = {
           enable = true, -- false will disable the whole extension
           disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
+            local max_filesize = 1024 * 1024 -- 1 MB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
               return true
