c3aeef65db141da374b8cf61c924739fba98a972
Waynerv <wei.xie@woqutech.com>
2023-12-05 15:33:35 +0800

    chore: do regular backup

--- update-alacritty-icon.sh
+++ update-alacritty-icon.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 
 icon_path=/Applications/Alacritty.app/Contents/Resources/alacritty.icns
 if [ ! -f "$icon_path" ]; then
-  echo "Can't find existing icon, make sure Alacritty is installed"
-  exit 1
+    echo "Can't find existing icon, make sure Alacritty is installed"
+    exit 1
 fi
 
 echo "Backing up existing icon"
@@ -14,11 +14,11 @@ mv "$icon_path" "$icon_path.backup-$hash"
 
 new_icon_path=alacritty/.config/alacritty/Alacritty.icns
 if [ ! -f "$new_icon_path" ]; then
-  echo "Can't find new icon"
-  exit 1
+    echo "Can't find new icon"
+    exit 1
 fi
 echo "Applying replacement icon"
-cp -f "$new_icon_path"  "$icon_path"
+cp -f "$new_icon_path" "$icon_path"
 
 touch /Applications/Alacritty.app
 killall Finder
