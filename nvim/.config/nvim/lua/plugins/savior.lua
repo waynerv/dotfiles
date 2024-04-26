38e6a4d9cf1c9f7abf71b42ce73ec92066f6b1cd
Waynerv <wei.xie@woqutech.com>
2024-04-26 19:52:32 +0800

    nvim: add plugin savior.nvim

---
+++ nvim/.config/nvim/lua/plugins/savior.lua
@@ -0,0 +1,9 @@
+local M = {
+  "willothy/savior.nvim",
+  commit = "b5ab8a692ab1b9ed911b7e43237f84d294fd6df3",
+  dependencies = { "j-hui/fidget.nvim" },
+  event = { "InsertEnter", "TextChanged" },
+  config = true,
+}
+
+return M
