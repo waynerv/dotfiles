a241bbb22de2a09efe7acfd32120d5ebd343b3cb
Waynerv <wei.xie@woqutech.com>
2026-04-03 16:18:47 +0800

    nvim: fix broken config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     "HiPhish/rainbow-delimiters.nvim",
-    commit = "3277ad5f96eb03c9d618c88e24f683e4364e578c",
+    commit = "50080ed0f44dbc18ae13b81278a01b951a06127a",
     event = { "BufReadPost", "BufNewFile" },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
